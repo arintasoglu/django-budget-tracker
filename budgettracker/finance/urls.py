64698a0834a9e6e09f328cf34296f658a414c4bc
arintasoglu <tasoglu@rhrk.uni-kl.de>
2025-12-19 23:18:29 +0100

    feat: add user authentication (login, logout, registration)

--- budgettracker/finance/urls.py
+++ budgettracker/finance/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.anmelden, name="anmelden"),
+    path('registrieren/', views.registrieren, name="registrieren"),
+    path('abmelden/', views.abmelden, name="abmelden"),
+    path('navbar/', views.navbar, name="navbar"),
 
 ]
